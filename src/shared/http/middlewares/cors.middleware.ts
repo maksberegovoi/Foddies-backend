a53def146f9928ff352504e6f8258bd064d184fa
Dmytro Popovskyi <popovskiy96@gmail.com>
2026-03-11 19:11:17 +0100

    uncomment cors allowedHeaders and methods

--- src/shared/http/middlewares/cors.middleware.ts
+++ src/shared/http/middlewares/cors.middleware.ts
@@ -11,8 +11,7 @@ const parseOrigins = (originsRaw: string) => {
 
 export const corsMiddleware = cors({
     origin: parseOrigins(env.CORS_ALLOWED_ORIGINS),
-    credentials: true
-    // TODO: uncomment and update list of allowed methods and headers before presentation
-    // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization']
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 })
