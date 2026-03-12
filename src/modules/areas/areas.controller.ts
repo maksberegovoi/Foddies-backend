06fb8edaccb43bf19a83c01e3aef5fd416a5012a
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-03-12 16:53:01 +0200

    feat: remove redundant try/catch in controller

--- src/modules/areas/areas.controller.ts
+++ src/modules/areas/areas.controller.ts
@@ -1,16 +1,17 @@
-import type { NextFunction, Request, Response } from 'express'
+import type { Request, Response } from 'express'
 import AreasService from './areas.service'
+import type { ApiResponse } from '../../shared/http/types/api-response.interface'
+import type { AreasResponseDto } from './dto/areas-response.dto'
 
 class AreasController {
     constructor(private readonly areasService: AreasService) {}
 
-    getAll = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const areas = await this.areasService.getAll()
-            return res.json(areas)
-        } catch (error) {
-            next(error)
-        }
+    getAll = async (
+        req: Request,
+        res: Response<ApiResponse<AreasResponseDto>>
+    ) => {
+        const areas = await this.areasService.getAll()
+        return res.json({ data: areas })
     }
 }
 
