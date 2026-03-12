0268a450fa8be594b9958d484a7af2f72af88eb3
AnnDe <anneta.liss@gmail.com>
2026-03-12 14:46:40 +0100

    prettier

--- src/modules/areas/areas.controller.ts
+++ src/modules/areas/areas.controller.ts
@@ -1,17 +1,17 @@
-import type { NextFunction, Request, Response } from 'express';
-import  AreasService from './areas.service';
+import type { NextFunction, Request, Response } from 'express'
+import AreasService from './areas.service'
 
 class AreasController {
     constructor(private readonly areasService: AreasService) {}
 
     getAll = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const areas = await this.areasService.getAll();
-            return res.json(areas);
+            const areas = await this.areasService.getAll()
+            return res.json(areas)
         } catch (error) {
-            next(error);
+            next(error)
         }
-    };
+    }
 }
 
-export const areasController = new AreasController(new AreasService());
+export const areasController = new AreasController(new AreasService())
