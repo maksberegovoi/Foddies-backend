26289239f8f35e804aafe0b79fd70f979c15e81b
KalyanovAnton <kalyanov.anton@gmail.com>
2026-03-12 22:26:54 +0100

    add createdAt

--- src/modules/testimonials/testimonials.service.ts
+++ src/modules/testimonials/testimonials.service.ts
@@ -2,14 +2,17 @@ import prisma from '../../prisma'
 import { type TestimonialDto } from './dto/testimonials.dto'
 
 class TestimonialsService {
-    async getTestimonials(limit: number = 10): Promise<TestimonialDto[]> {
+    async getTestimonials(limit: number = 3): Promise<TestimonialDto[]> {
         return prisma.testimonial.findMany({
             select: {
                 id: true,
                 owner: true,
                 text: true
             },
-            take: limit
+            take: limit,
+            orderBy: {
+                createdAt: 'desc'
+            }
         })
     }
 }
