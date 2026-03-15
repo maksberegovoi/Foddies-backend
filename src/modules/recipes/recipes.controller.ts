b57e7eac6c51f79458d858661acda3054ee8534e
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-03-15 13:57:11 +0200

    fix pagination

--- src/modules/recipes/recipes.controller.ts
+++ src/modules/recipes/recipes.controller.ts
@@ -18,6 +18,7 @@ class RecipesController {
     ) => {
         const query = getRecipesQuerySchema.parse(req.query)
         const result = await this.recipesService.getAll(query)
+
         res.json({
             data: result.items,
             meta: {
@@ -27,18 +28,23 @@ class RecipesController {
             }
         })
     }
+
     getById = async (req: Request, res: Response<ApiResponse<RecipeDto>>) => {
         const { id } = idParamSchema.parse(req.params)
         const recipe = await this.recipesService.getById(id)
+
         res.json({ data: recipe })
     }
+
     getPopular = async (
         req: Request,
         res: Response<ApiResponse<RecipeCardDto[]>>
     ) => {
         const recipes = await this.recipesService.getPopular()
+
         res.json({ data: recipes })
     }
+
     getUserRecipes = async (
         req: Request,
         res: Response<ApiResponsePaginated<RecipeCardDto>>
@@ -47,8 +53,10 @@ class RecipesController {
         const query = paginationQuery.parse(req.query)
         const { items: recipes, ...meta } =
             await this.recipesService.getUserRecipes(query, userId)
+
         res.json({ data: recipes, meta })
     }
+
     getFavorite = async (
         req: Request,
         res: Response<ApiResponsePaginated<RecipeCardDto>>
@@ -57,8 +65,10 @@ class RecipesController {
         const query = paginationQuery.parse(req.query)
         const { items: recipes, ...meta } =
             await this.recipesService.getFavorite(query, userId)
+
         res.json({ data: recipes, meta })
     }
+
     addFavorite = async (req: Request, res: Response) => {
         const { id: userId } = req.user
         const { id: recipeId } = idParamSchema.parse(req.params)
@@ -66,12 +76,15 @@ class RecipesController {
 
         res.sendStatus(204)
     }
+
     removeFavorite = async (req: Request, res: Response) => {
         const { id: userId } = req.user
         const { id: recipeId } = idParamSchema.parse(req.params)
         await this.recipesService.removeFavorite(recipeId, userId)
+
         res.sendStatus(204)
     }
+
     create = async (
         req: Request,
         res: Response<ApiResponse<Pick<RecipeDto, 'id'>>>
@@ -80,12 +93,15 @@ class RecipesController {
         const file = req.file
         const data = createRecipeSchema.parse(req.body)
         const recipeId = await this.recipesService.create(data, userId, file)
+
         res.json({ data: recipeId })
     }
+
     delete = async (req: Request, res: Response) => {
         const { id: userId } = req.user
         const { id: recipeId } = idParamSchema.parse(req.params)
         await this.recipesService.delete(recipeId, userId)
+
         res.sendStatus(204)
     }
 }
